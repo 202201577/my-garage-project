c9fc5c9db09fcf9f7b2fb93ebf992d37946713dd
202201577 <alhanoof.t29@gmail.com>
2026-05-08 01:56:07 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA1TicQrYLv7ypC7cJqGUasQfC2e5L6Es8",
+  authDomain: "my---garage.firebaseapp.com",
+  projectId: "my---garage",
+  storageBucket: "my---garage.firebasestorage.app",
+  messagingSenderId: "52476419320",
+  appId: "1:52476419320:web:1742f544a775e2864baf0d",
+  measurementId: "G-Q9MZ7TS0XT"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
